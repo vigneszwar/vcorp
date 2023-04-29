a5f2467d0bd9f310364e5f306ee9878188553ab5
vigneszwar <vignezwar@gmail.com>
2023-04-29 13:18:58 +0530

    spring boot

---
+++ src/main/java/com/vcorp/backendservice/BackendserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vcorp.backendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcorp/backendservice/BackendserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcorp.backendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
